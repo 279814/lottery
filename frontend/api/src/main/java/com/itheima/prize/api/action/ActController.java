fba25ff143e79e6339b4e7fd09adb3e43e117c40
LLL <2798142644@qq.com>
2026-04-14 18:14:22 +0800

    恢复tomcat默认设计

--- frontend/api/src/main/java/com/itheima/prize/api/action/ActController.java
+++ frontend/api/src/main/java/com/itheima/prize/api/action/ActController.java
@@ -44,93 +44,159 @@ public class ActController {
             @ApiImplicitParam(name="gameid",value = "活动id",example = "1",required = true)
     })
     public ApiResult<Object> act(@PathVariable int gameid, HttpServletRequest request){
-        //获取活动信息
-        Object game = redisUtil.get(RedisKeys.INFO + gameid);
-        if (game == null) {
-            return new ApiResult(-1,"活动未开始",null);
-        }
-        CardGame gameInfo = (CardGame) game;
+        Date now = new Date();
+        //获取活动基本信息
+        CardGame game = (CardGame) redisUtil.get(RedisKeys.INFO+gameid);
         //判断活动是否开始
-        if (gameInfo.getStarttime().getTime() > System.currentTimeMillis()) {
+        //如果活动信息还没加载进redis，无效
+        //如果活动已经加载，预热完成，但是开始时间 > 当前时间，也无效
+        if (game == null || game.getStarttime().after(now)){
             return new ApiResult(-1,"活动未开始",null);
         }
-        //判断活动是否结束
-        if (gameInfo.getEndtime().getTime() < System.currentTimeMillis()) {
+        //判断活动是否已结束
+        if (now.after(game.getEndtime())){
             return new ApiResult(-1,"活动已结束",null);
         }
-        //判断用户是否登录
+        //获取当前用户
         HttpSession session = request.getSession();
-        Object user = session.getAttribute("user");
-        if (user == null) {
-            return new ApiResult(-1,"未登录",null);
-        }
-
-        CardUser userInfo = (CardUser) user;
-        if(redisUtil.setNx(RedisKeys.USERGAME + gameid + "_" + userInfo.getId(), 1)){
-            //mq异步通知
-            CardUserGame cardUserGame = new CardUserGame();
-            cardUserGame.setUserid(userInfo.getId());
-            cardUserGame.setGameid(gameid);
-            cardUserGame.setCreatetime(new Date());
-            //RabbitMq传输对象的时候，可以使用FastJson将对象转为字符串后传输
-            String message = JSON.toJSONString(cardUserGame);
-            rabbitTemplate.convertAndSend(RabbitKeys.EXCHANGE_DIRECT,RabbitKeys.QUEUE_PLAY,message);
-        }
-        //获取用户level
-        Integer level = userInfo.getLevel();
-        //获取活动策略
-        Integer maxGoal = (Integer)redisUtil.hget(RedisKeys.MAXGOAL + gameid, level + "");
-        Integer maxEnter = (Integer)redisUtil.hget(RedisKeys.MAXENTER + gameid, level + "");
-        if(!redisUtil.hasKey(RedisKeys.USERENTER + gameid + "_" + userInfo.getId())){
-            //活动当前还有多少时间
-            long enterTime = gameInfo.getEndtime().getTime() - System.currentTimeMillis();
-            //转成秒
-            long enterTimeSeconds = enterTime / 1000;
-            redisUtil.set(RedisKeys.USERENTER + gameid + "_" + userInfo.getId(), 0, enterTimeSeconds + 1);
-            redisUtil.set(RedisKeys.USERHIT + gameid + "_" + userInfo.getId(), 0, enterTimeSeconds + 1);
-        }
-        //判断抽奖次数
-        if(maxGoal != 0){
-            //判断用户是否超过最大抽奖次数
-            if((Integer)redisUtil.get(RedisKeys.USERENTER + gameid + "_" + userInfo.getId()) >= maxEnter){
-                return new ApiResult(-1,"您的抽奖次数已用完",null);
-            }
-        }
-        //判断中奖次数
-        if(maxGoal != 0){
-            //判断用户是否超过最大中奖次数
-            if((Integer)redisUtil.get(RedisKeys.USERHIT + gameid + "_" + userInfo.getId()) >= maxGoal){
-                return new ApiResult(-1,"您已达到最大中奖数",null);
-            }
-        }
-
-
-        Long token = luaScript.tokenCheck(RedisKeys.TOKENS+gameid,String.valueOf(new Date().getTime()));
-        if(token == 0){
-            //抽奖次数+1
-            redisUtil.incr(RedisKeys.USERENTER + gameid + "_" + userInfo.getId(), 1);
-            return new ApiResult(-1,"奖品已抽光",null);
-        }else if(token == 1){
-            redisUtil.incr(RedisKeys.USERENTER + gameid + "_" + userInfo.getId(), 1);
-            return new ApiResult(0,"未中奖",null);
+        CardUser user = (CardUser) session.getAttribute("user");
+        if (user == null){
+            return new ApiResult(-1,"未登陆",null);
         }else{
-            //token有效，中奖！
-            redisUtil.incr(RedisKeys.USERENTER + gameid + "_" + userInfo.getId(), 1);
-            redisUtil.incr(RedisKeys.USERHIT + gameid + "_" + userInfo.getId(), 1);
-            //奖品信息
-            CardProduct cardProduct = (CardProduct)redisUtil.get(RedisKeys.TOKEN + gameid + "_" + token);
-
-            //mq
-            CardUserHit cardUserHit = new CardUserHit();
-            cardUserHit.setUserid(userInfo.getId());
-            cardUserHit.setGameid(gameid);
-            cardUserHit.setProductid(cardProduct.getId());
-            cardUserHit.setHittime(new Date());
-            String msg = JSON.toJSONString(cardUserHit);
-            rabbitTemplate.convertAndSend(RabbitKeys.EXCHANGE_DIRECT,RabbitKeys.QUEUE_HIT,msg);
-
-            return new ApiResult(1,"恭喜中奖",cardProduct);
+            //第一次抽奖，发送消息队列，用于记录参与的活动（redis分布式锁）
+            if (redisUtil.setNx(RedisKeys.USERGAME+user.getId()+"_"+gameid,1)){
+                //持久化抽奖记录，扔给消息队列处理
+                CardUserGame userGame = new CardUserGame();
+                userGame.setUserid(user.getId());
+                userGame.setGameid(gameid);
+                userGame.setCreatetime(new Date());
+                rabbitTemplate.convertAndSend(RabbitKeys.EXCHANGE_DIRECT,RabbitKeys.QUEUE_PLAY, JSON.toJSONString(userGame));
+            }
         }
+
+        //用户可抽奖次数
+        Integer enter = (Integer) redisUtil.get(RedisKeys.USERENTER+gameid+"_"+user.getId());
+        if (enter == null){
+            enter = 0;
+            redisUtil.set(RedisKeys.USERENTER+gameid+"_"+user.getId(),enter,(game.getEndtime().getTime() - now.getTime())/1000);
+        }
+        //根据会员等级，获取本活动允许的最大抽奖次数
+        Integer maxenter = (Integer) redisUtil.hget(RedisKeys.MAXENTER+gameid,user.getLevel()+"");
+        //如果没设置，默认为0，即：不限制次数
+        maxenter = maxenter==null ? 0 : maxenter;
+        //次数对比
+        if (maxenter > 0 && enter >= maxenter){
+            //如果达到最大次数，不允许抽奖
+            return new ApiResult(-1,"您的抽奖次数已用完",null);
+        }else{
+            redisUtil.incr(RedisKeys.USERENTER+gameid+"_"+user.getId(),1);
+        }
+
+
+        //用户已中奖次数
+        Integer count = (Integer) redisUtil.get(RedisKeys.USERHIT+gameid+"_"+user.getId());
+        if (count == null){
+            count = 0;
+            redisUtil.set(RedisKeys.USERHIT+gameid+"_"+user.getId(),count,(game.getEndtime().getTime() - now.getTime())/1000);
+        }
+        //根据会员等级，获取本活动允许的最大中奖数
+        Integer maxcount = (Integer) redisUtil.hget(RedisKeys.MAXGOAL+gameid,user.getLevel()+"");
+        //如果没设置，默认为0，即：不限制次数
+        maxcount = maxcount==null ? 0 : maxcount;
+        //次数对比
+        if (maxcount > 0 && count >= maxcount){
+            //如果达到最大次数，不允许抽奖
+            return new ApiResult(-1,"您已达到最大中奖数",null);
+        }
+
+
+        //以上校验全部过关，进入下一步：拿token
+        //也就是决定中不中最关键的一步，拿到合法token就中奖
+        Long token;
+        switch (game.getType()) {
+            //时间随机
+            case 1:
+                //随即类比较麻烦，按设计时序图走
+
+                //java调redis，有原子性问题！
+//                token = (Long) redisUtil.leftPop(RedisKeys.TOKENS+gameid);
+//                if (token == null){
+//                    //令牌已用光，说明奖品抽光了
+//                    return new ApiResult(-1,"奖品已抽光",null);
+//                }
+//                //判断令牌时间戳大小，即是否中奖
+//                //这里记住，取出的令牌要除以1000，参考job项目，令牌生成部分
+//                if (now.getTime() < token/1000){
+//                    //当前时间小于令牌时间戳，说明奖品未到发放时间点，放回令牌，返回未中奖
+//                    redisUtil.leftPush(RedisKeys.TOKENS+gameid,token);
+//                    return new ApiResult(0,"未中奖",null);
+//                }
+
+
+                //lua调redis
+                token = luaScript.tokenCheck(RedisKeys.TOKENS+gameid,String.valueOf(new Date().getTime()));
+                if(token == 0){
+                    return new ApiResult(-1,"奖品已抽光",null);
+                }else if(token == 1){
+                    return new ApiResult(0,"未中奖",null);
+                }
+
+                break;
+
+            case 2:
+
+                //瞬间秒杀类简单，直接获取令牌，有就中，没有就说明抢光了
+                token = (Long) redisUtil.leftPop(RedisKeys.TOKENS+gameid);
+                if (token == null){
+                    //令牌已用光，说明奖品抽光了
+                    return new ApiResult(-1,"奖品已抽光",null);
+                }
+
+                break;
+
+            case 3:
+
+                //幸运转盘类，先给用户随机剔除，再获取令牌，有就中，没有就说明抢光了
+                //一般这种情况会设置足够的商品，卡在随机上
+                Integer randomRate = (Integer) redisUtil.hget(RedisKeys.RANDOMRATE+gameid,user.getLevel()+"");
+                if (randomRate == null){
+                    randomRate = 100;
+                }
+                //注意这里的概率设计思路：
+                //每次请求取一个0-100之间的随机数，如果这个数没有落在范围内，直接返回未中奖
+                if( new Random().nextInt(100) > randomRate ){
+                    return new ApiResult(0,"未中奖",null);
+                }
+
+                token = (Long) redisUtil.leftPop(RedisKeys.TOKENS+gameid);
+                if (token == null){
+                    //令牌已用光，说明奖品抽光了
+                    return new ApiResult(-1,"奖品已抽光",null);
+                }
+
+                break;
+
+            default:
+                return new ApiResult(-1,"不支持的活动类型",null);
+
+        }//end switch
+
+
+        //以上逻辑走完，拿到了合法的token，说明很幸运，中奖了！
+        //抽中的奖品：
+        CardProduct product = (CardProduct) redisUtil.get(RedisKeys.TOKEN + gameid +"_"+token);
+        //中奖次数加1
+        redisUtil.incr(RedisKeys.USERHIT+gameid+"_"+user.getId(),1);
+        //投放消息给队列，中奖后的耗时业务，交给消息模块处理
+        CardUserHit hit = new CardUserHit();
+        hit.setGameid(gameid);
+        hit.setHittime(now);
+        hit.setProductid(product.getId());
+        hit.setUserid(user.getId());
+        rabbitTemplate.convertAndSend(RabbitKeys.EXCHANGE_DIRECT,RabbitKeys.QUEUE_HIT, JSON.toJSONString(hit));
+
+        //返回给前台中奖信息
+        return new ApiResult(1,"恭喜中奖",product);
 
     }
 
